e1f78639bfd441dedca3f7f2c8672a18063fc5ca
lucagrillo_sxc <luca.grillo@sixthcontinent.com>
2020-02-10 23:38:57 +0100

    first version ok

--- app.js
+++ app.js
@@ -12,9 +12,45 @@ const server = http.createServer((req, res) => {
   //Set the response HTTP header with HTTP status and Content type
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+  res.end('Fuck World\n');
 
 });
+
+var welcome = ctx => ctx.reply('Welcome!')
+
+var write = (ctx) => firestore.write(ctx.message.from, ctx.message.text)
+
+var last = (ctx) => {
+  firestore.last(doc => {
+      console.log(`last: ${JSON.stringify(doc.story.id)} ${JSON.stringify(doc.text)}`)
+      ctx.reply(doc.text)
+  })
+}
+
+var read = (ctx) => {
+  
+  firestore.read(arr => {
+    ctx.reply(arr.join('\n'))
+  })
+
+}
+
+var story = (ctx) => {
+
+  let storyName = ctx.state.command.args
+  if(storyName == '') {
+    ctx.reply(`enter story name`)
+  
+  } else {
+    created = firestore.setStory(storyName, created => {
+      if(created) {
+        ctx.reply(`story "${storyName}" created`)
+      } else {
+        ctx.reply(`you joined "${storyName}" story`)
+      }
+    });
+  }
+}
 
 //listen for request on port 3000, and as a callback function have the port listened on logged
 server.listen(port, hostname, () => {
@@ -25,62 +61,14 @@ server.listen(port, hostname, () => {
   const bot = new Telegraf(process.env.BOT_TOKEN)
   bot.use(commandParts());
   
-  firestore.init();
+  // firestore.init();
   
-  bot.start((ctx) => {
+  bot.start(welcome)
 
-    ctx.reply('Welcome!');
-
-  })
-
-  bot.command('story', (ctx) => {
-
-    let storyName = ctx.state.command.args
-    if(storyName == '') {
-      ctx.reply(`enter story name`)
-    
-    } else {
-      created = firestore.setStory(storyName, created => {
-        if(created) {
-          ctx.reply(`story "${storyName}" created`)
-        } else {
-          ctx.reply(`you joined "${storyName}" story`)
-        }
-      });
-    }
-  })
-  
-  bot.command('read', ctx => {
-
-    let arr = []
-    let story_ref = firestore.read(ctx.message.from, snapshot => 
-      snapshot.forEach(doc => 
-        arr.push(doc.data().text)
-    ));
-
-    story_ref.get().then(doc => {
-      ctx.reply(`
-        [ ${doc.data().name} ]\n------------------------ \n${arr.join('\n')}`)
-    })
-  })
-
-  bot.command('last', ctx => {
-
-    firestore.last(doc => {
-        
-      firestore.story_id = doc.story.id
-        console.log(JSON.stringify(doc.story.id))
-        ctx.reply(doc.text)
-
-    })
-    
-  });
-
-  bot.on('text', ctx => {
-
-    firestore.write(ctx.message.from, ctx.message.text)
-
-  })
+  bot.command('story', story)
+  bot.command('read', read)
+  bot.command('last', last)
+  bot.on('text', write)
   
   bot.launch()
 
--- firestore.js
+++ firestore.js
@@ -1,27 +1,22 @@
+
+const admin = require('firebase-admin');
+let serviceAccount = require('./serviceAccountKey.json');
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://cadavre-exquis-215311.firebaseio.com"
+});
+
+db = admin.firestore()
+
+var getIncompleteStory = (callback) => {
+    db.collection('stories')
+        .where('complete', '==', false)
+        .limit(1).get()
+        .then(snapshot => snapshot.forEach(doc => callback(doc.id)))
+    }
+
 module.exports = {
-
-    init: () => {
-
-        const admin = require('firebase-admin');
-        let serviceAccount = require('./serviceAccountKey.json');
-
-        admin.initializeApp({
-            credential: admin.credential.cert(serviceAccount),
-            databaseURL: "https://cadavre-exquis-215311.firebaseio.com"
-        });
-        
-        db = admin.firestore()
-        db.collection('stories')
-            .where('complete', '==', false)
-            .limit(1)
-            .get()
-            .then(snapshot => {
-                snapshot.forEach(doc => {
-                    this.story_id = doc.id
-                })
-            })
-
-    },
     setStory: (storyName, callback) => {
 
         let story_ref = db.collection('stories').doc();
@@ -33,7 +28,7 @@ module.exports = {
                     complete: false
                 });
             }
-            this.story_id = story_ref.id
+            story_id = story_ref.id
             callback(snapshot.empty);
         });
     
@@ -55,46 +50,45 @@ module.exports = {
                         ))
                   });
             })
+    },
+    read: (callback) => {
         
-    },
-    getIncompleteStoryId: (callback) => {
-        db.collection('stories')
-            .where('complete', '==', false)
-            .limit(1)
-            .get()
-            .then(snapshot => {
-                snapshot.forEach(doc => {
-                    callback(doc.data().id)
+        getIncompleteStory(story_id => {        
+
+            let story_ref = db.collection('stories').doc(`${story_id}`);
+
+            var arr = []
+            db.collection(`chapters`)
+                .where('story', '==', story_ref)
+                .orderBy('timestamp')
+                .get()
+                .then(snapshot => {
+                    if(snapshot.size != 0) {
+                        snapshot.forEach(doc => {
+                            arr.push(doc.data().text)
+                        });
+                        callback(arr)
+                    }
                 })
-            })
-    },
-    read: (user, callback) => {
-        let user_ref = db.collection('users').doc(`${user.id}`);
-        let story_ref = db.collection('stories').doc(`${this.story_id}`);
-
-        db.collection(`chapters`)
-            .where('created_by', '==', user_ref)
-            .where('story', '==', story_ref)
-            .orderBy('timestamp')
-            .get()
-            .then(callback)
-
-        return story_ref
+        })
     },
     write: (user, text) => {
 
-        console.log(`story_id: ${this.story_id}`)
-        let user_ref = db.collection('users').doc(`${user.id}`);
-        user_ref.set(user);
+        getIncompleteStory(story_id => {       
 
-        let story_ref = db.collection('stories').doc(`${this.story_id}`);
-        let docRef = db.collection('chapters').doc(`${Date.now()}`)
+            let user_ref = db.collection('users').doc(`${user.id}`);
+            user_ref.set(user);
 
-        docRef.set({
-            created_by: user_ref,
-            story: story_ref,
-            text: text,
-            timestamp: Date.now()
+            let story_ref = db.collection('stories').doc(`${story_id}`);
+            let docRef = db.collection('chapters').doc(`${Date.now()}`)
+
+            docRef.set({
+                created_by: user_ref,
+                story: story_ref,
+                text: text,
+                timestamp: Date.now()
+            })
+
         })
 
     }
